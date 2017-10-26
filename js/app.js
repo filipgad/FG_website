2f8f7cb802a4e43066d12789df0d2265941f16ed
Filip Godziński <godzinski.filip@gmail.com>
2017-10-26 23:23:25 +0200

    Added contact section and some parallax animation in header and contact section

--- js/app.js
+++ js/app.js
@@ -12,9 +12,6 @@
 ( function( $ ) {
     // Init Skrollr
     var s = skrollr.init({
-        render: function(data) {
-            //Debugging - Log the current scroll position.
-            //console.log(data.curTop);
-        }
+        forceHeight: false
     });
 } )( jQuery );
